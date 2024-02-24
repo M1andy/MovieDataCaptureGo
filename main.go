56b68f83e14efe2281930a634c6a21278a5173fe
M1andy <M1andy@mail.dlut.edu.cn>
2024-02-24 17:30:30 +0800

    test: test javdb crawler

--- main.go
+++ main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
+	"fmt"
+
+	. "MovieDataCaptureGo/internal/config"
 	_ "MovieDataCaptureGo/internal/crawler"
 	avCawler "MovieDataCaptureGo/internal/crawler"
+	"MovieDataCaptureGo/internal/glob"
 	. "MovieDataCaptureGo/internal/logger"
-	"fmt"
 )
 
 func main() {
 	Logger.Infoln("Reading config finished!")
-	//glob.VideoFiles(c.CFG)
-
-	number := "OFJE-377"
-	crawler := avCawler.NewJavbusCrawlerFactory("www.javbus.com")
-	err := crawler.CrawlNumber(number)
+	_, err := glob.JAVFiles(CFG.Main.SourceDirectory)
+	if err != nil {
+		Logger.Debugln(err)
+	}
+	number := "IPX-901"
+	crawler := avCawler.NewJavdbCrawlerFactory("www.javdb524.com")
+	err = crawler.CrawlNumber(number)
 	if err != nil {
 		Logger.Debugln(err)
 	}
 
-	outlineCrawler := avCawler.NewAirwikiCrawler("www.airav.wiki")
-	err = outlineCrawler.CrawlNumber(number)
-	if err != nil {
-		Logger.Debugln(err)
-	}
+	Logger.Infoln("Waiting for exit, press any key...")
 	fmt.Scanln()
 }
