c39915bf446315c89758292f4f8d8483caf13ba6
M1andy <M1andy@mail.dlut.edu.cn>
2024-02-24 17:29:58 +0800

    chunk: code format

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -1,10 +1,11 @@
 package proxy
 
 import (
-	c "MovieDataCaptureGo/internal/config"
-	. "MovieDataCaptureGo/internal/logger"
 	"github.com/gocolly/colly/v2"
 	"github.com/gocolly/colly/v2/proxy"
+
+	c "MovieDataCaptureGo/internal/config"
+	. "MovieDataCaptureGo/internal/logger"
 )
 
 var ProxyFunc colly.ProxyFunc
@@ -14,6 +15,7 @@ func newProxyFunc(cfg *c.Config) colly.ProxyFunc {
 		return nil
 	}
 	ps, err := proxy.RoundRobinProxySwitcher(c.CFG.Proxy.URL...)
+	Logger.Debugf("Proxy List: %s", c.CFG.Proxy.URL)
 	if err != nil {
 		Logger.Warnln(err)
 	}
